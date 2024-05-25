18b61cbc92b7fef899d63f2d28e7cb2f251ba4f9
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-05-24 22:35:58 -0300

    update

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,23 +1,35 @@
+"use client";
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
 }
 
-const BarbershopItem = async ({ barbershop }: BarbershopItemProps) => {
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
+
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
+
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="p-1 py-1">
         <div className="px-1 relative h-[159px] w-full">
           <div className="absolute top-3 left-3 z-50">
-            <Badge variant="secondary" className="flex items-center gap-1 top-3 left-3">
+            <Badge
+              variant="secondary"
+              className="flex items-center gap-1 top-3 left-3"
+            >
               <StarIcon size={12} className="fill-primary text-primary" />
-              <span>5,0</span> 
+              <span>5,0</span>
             </Badge>
           </div>
           <Image
@@ -39,7 +51,7 @@ const BarbershopItem = async ({ barbershop }: BarbershopItemProps) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap mt-2">
             {barbershop.address}
           </p>
-          <Button variant="secondary" className="w-full mt-3">
+          <Button variant="secondary" className="w-full mt-3" onClick={handleBookingClick}>
             Reservar
           </Button>
         </div>
